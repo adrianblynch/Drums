1dd5d49a88b667e2fd66252fbd55b3a3e8579f80
Adrian Lynch <adi_ady_ade@hotmail.com>
2016-12-10 11:07:18 +0000

    ✍ Routeless

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Match, Link } from 'react-router'
 import Drums from './Drums'
 
 export default class App extends Component {
 
 	render() {
 		return (
-			<BrowserRouter>
-				<div>
-					<ul>
-						<li><Link to="drums">Drums</Link></li>
-					</ul>
-					<Match pattern="*drums" component={ Drums } />
-				</div>
-			</BrowserRouter>
+			<Drums />
 		)
 	}
 }
